15fbde0532519df75093027eee7987d85c04909a
Kushami Neko <missing10630@gmail.com>
2016-11-30 00:49:16 +0800

    modify debug macro

--- src/debug_macro.h
+++ src/debug_macro.h
@@ -2,23 +2,33 @@
 #define DEBUG_MACRO_H
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifdef DEBUG
 
-#define REQUIRE(expr) assert(expr)
 #define ENSURE(expr) assert(expr)
+
+#define ENSURE_MSG(expr, errMsg, ...) \
+  ({                                  \
+    if ((expr) != true) {             \
+      printf(errMsg, ##__VA_ARGS__);  \
+                                      \
+      exit(EXIT_FAILURE);             \
+    }                                 \
+  })
 
 #define DEBUG_MESSAGE(m, ...) printf(m, ##__VA_ARGS__)
 
 #else
 
-#define REQUIRE(expr) \
-  do {                \
-  } while (0)
-
 #define ENSURE(expr) \
   do {               \
+  } while (0)
+
+#define ENSURE_MSG(expr, errMsg, ...) \
+  do {                                \
   } while (0)
 
 #define DEBUG_MESSAGE(m, ...) \
